226e1cf76046feb2d1cfa406aa16101774bd53f1
georgio-khalil <georgiokhalil44@gmail.com>
2023-01-05 21:34:58 +0000

    Solving challenges 01_07 and 01_08

--- 01_restaurant/challenges/01_07_attendevent_ch.sql
+++ 01_restaurant/challenges/01_07_attendevent_ch.sql
@@ -1,4 +1,17 @@
--- Register a customer for our Anniversary event.
+-- Challenge 01_07: 'Register a customer for our Anniversary event' solved by Georgio Khalil
 
 -- The customer 'atapley2j@kinetecoinc.com' will be in
 -- attendance, and will bring 3 friends.
+
+-- Solution:
+
+-- Use email address to look up customer and get customer ID as a subquery
+INSERT INTO CustomersResponses
+VALUES (
+  (SELECT CustomerID 
+    FROM Customers
+    WHERE Email = 'atapley2j@kinetecoinc.com'),
+  4);
+
+--Check updated table
+SELECT * FROM CustomersResponses;
--- 01_restaurant/challenges/01_08_findreservation_ch.sql
+++ 01_restaurant/challenges/01_08_findreservation_ch.sql
@@ -1,7 +1,13 @@
--- Find the reservation information for a customer whose
--- name we aren't quite sure how to spell.
+-- Challenge 01_08: 'Find the reservation information for a customer whose name we aren't quite sure how to spell' solved by Georgio Khalil
 
 -- Variations of the name include:
 -- Stevensen, Stephensen, Stevenson, Stephenson, Stuyvesant
 
 -- There are four people in the party. Today is June 14th.
+
+-- Solution:
+
+SELECT * FROM Reservations
+JOIN Customers ON Reservations.CustomerID = Customers.CustomerID
+WHERE Customers.LastName LIKE 'St%e%s%'
+  AND date(Reservations.Date) = '2022-06-14'
