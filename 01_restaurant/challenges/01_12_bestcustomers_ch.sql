f28f85574e8f5d38c6fa1734d62ad3dfa9e43c9b
georgio-khalil <georgiokhalil44@gmail.com>
2023-01-06 20:26:48 +0000

    Solving challenge 01_12

--- 01_restaurant/challenges/01_12_bestcustomers_ch.sql
+++ 01_restaurant/challenges/01_12_bestcustomers_ch.sql
@@ -1,3 +1,13 @@
--- Identify a few customers who have ordered delivery
+-- Challenge 01_12: Identify a few customers who have ordered delivery
 -- from the restaurant the most often, so we can send
 -- them a promotional coupon.
+-- solved by Georgio Khalil
+
+-- Solution:
+
+SELECT Customers.FirstName, Customers.LastName, Customers.Email, count(Orders.OrderID) AS NumberOfOrders
+FROM Customers
+JOIN Orders ON Customers.CustomerID = Orders.CustomerID
+GROUP BY orders.CustomerID
+ORDER BY NumberOfOrders DESC
+LIMIT 6;
