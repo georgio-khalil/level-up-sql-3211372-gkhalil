14e8667303202713ac7c25ccae1c4f321b571ea5
georgio-khalil <georgiokhalil44@gmail.com>
2023-01-06 19:58:16 +0000

    Solving challenge 01_10

--- 01_restaurant/challenges/01_10_takeorder_ch.sql
+++ 01_restaurant/challenges/01_10_takeorder_ch.sql
@@ -1,5 +1,4 @@
--- Enter a customer's delivery order into our database, 
--- and provide the total cost of the items ordered.
+-- Challenge 01_1: 'Enter a customer's delivery order into our database' and provide the total cost of the items ordered' solved by Georgio Khalil
 
 -- Use this order information:
 -- Customer: Loretta Hundey, at 6939 Elka Place
@@ -7,3 +6,34 @@
 -- 1 Tropical Blue Smoothie
 -- Delivery date and time: September 20, 2022 @ 2PM (14:00)
 -- There are no taxes or other fees.
+
+-- Solution:
+
+-- Find the customer in our database:
+SELECT * from Customers
+WHERE FirstName = 'Loretta';
+
+-- Create order record from customer ID 70:
+INSERT INTO Orders (CustomerID, OrderDate)
+VALUES (70, '2022-09-20 14:00:00');
+
+-- Check order ID
+SELECT * FROM Orders
+WHERE CustomerID = 70
+ORDER BY OrderDate DESC;
+
+-- Add items to order ID 1001:
+INSERT INTO OrdersDishes (OrderID,DishID)
+VALUES (1001,(SELECT DishID FROM Dishes WHERE Name = 'House Salad'));
+INSERT INTO OrdersDishes (OrderID,DishID)
+VALUES (1001,(SELECT DishID FROM Dishes WHERE Name = 'Mini Cheeseburgers'));
+INSERT INTO OrdersDishes (OrderID,DishID)
+VALUES (1001,(SELECT DishID FROM Dishes WHERE Name = 'Tropical Blue Smoothie'));
+
+-- Check full order with items
+SELECT OrdersDishes.OrderID, OrdersDishes.DishID, Dishes.Name, Dishes.Price FROM OrdersDishes
+JOIN Dishes ON OrdersDishes.DishID = Dishes.DishID WHERE OrdersDishes.OrderID = 1001
+
+-- Calculate total cost
+SELECT sum(Dishes.Price) FROM OrdersDishes
+JOIN Dishes ON OrdersDishes.DishID = Dishes.DishID WHERE OrdersDishes.OrderID = 1001
