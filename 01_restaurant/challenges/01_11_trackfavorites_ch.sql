92ba67cd1416f8d2b2511975f79c7795b4ec78ae
georgio-khalil <georgiokhalil44@gmail.com>
2023-01-06 20:15:44 +0000

    Solving challenge 01_11

--- 01_restaurant/challenges/01_11_trackfavorites_ch.sql
+++ 01_restaurant/challenges/01_11_trackfavorites_ch.sql
@@ -1,4 +1,19 @@
--- Update information in the database.
+-- Challenge 01_11: 'Update information in the database' solved by Georgio Khalil
 
 -- Set Cleo Goldwater's favorite dish to
 -- the Quinoa Salmon Salad.
+
+-- Solution:
+
+-- Find customer in database:
+SELECT * FROM Customers
+WHERE FirstName = 'Cleo' and LastName = 'Goldwater'
+
+-- Check current favorite dish:
+SELECT * from Dishes
+WHERE DishID = 14
+
+-- Update favorite dish:
+UPDATE Customers
+SET FavoriteDish = (SELECT DishID FROM Dishes WHERE Name = 'Quinoa Salmon Salad')
+WHERE FirstName = 'Cleo' and LastName = 'Goldwater'
